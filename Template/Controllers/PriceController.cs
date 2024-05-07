bb390d742f67adb9434a5f6bdd7bd2f5911eb8da
SamirAshumov <tu6KYLC6Z@code.edu.az>
2024-05-07 11:49:20 +0400

    uploaded

---
+++ Template/Controllers/PriceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Template.Controllers
+{
+    public class PriceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Template/Controllers/WorkController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Template.Controllers
+{
+    public class WorkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
